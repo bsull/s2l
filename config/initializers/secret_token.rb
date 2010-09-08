67e49eab36ce7d5a1f2cc69e9d0651d8ce5e4fdb
Blair Sullivan <blairsull@gmail.com>
2010-09-08 07:46:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S2l::Application.config.secret_token = '251855771fed683ebf72a57b82c7236dfa5830edaf94199158b5d6cf5e89019c03d9168094f10f4e94a26c150a492ce3a854dbe954a010a582f250c0b778ed01'
